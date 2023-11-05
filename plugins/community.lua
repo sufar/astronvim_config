a074b1148ac4daecfdfadcde613bccc223b05b21
sufar <fozoto@163.com>
2023-11-05 21:08:03 +0800

    community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,7 +6,8 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.vue" },
-  { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.vue" },
+  -- { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.dart" },
 }
