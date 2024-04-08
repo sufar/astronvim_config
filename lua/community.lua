624cdd24370fd7b9e8618847abdc46d9980d70ba
sufar <fozoto@163.com>
2024-04-08 16:56:21 +0000

    v4

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,11 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
----@type LazySpec
 return {
+  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  -- example of importing a plugin
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- example of importing an entire language pack
+  -- these packs can set up things such as Treesitter, Language Servers, additional language specific plugins, and more!
+  { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.python" },
 }
