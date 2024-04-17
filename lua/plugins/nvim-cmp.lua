4fe7f025bba350d1f40962cdae125eb7f8d06545
sufar <fozoto@163.com>
2024-04-17 13:01:57 +0000

    keymap fixed error

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,6 +1,5 @@
 -- https://docs.astronvim.com/recipes/cmp/
 return {
   "hrsh7th/nvim-cmp",
-    opts = function(_, config) config.mapping["<CR>"] = require("cmp").mapping.confirm { select = true } end,
-    }
+  opts = function(_, config) config.mapping["<CR>"] = require("cmp").mapping.confirm { select = true } end,
 }
